89288e531a69f76d291e473b362ba55a43b090a0
Flintpop <79105055+Flintpop@users.noreply.github.com>
2023-06-20 11:44:52 +0200

    Update main.py et autre

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from update_project import check_for_update_and_restart
+from hehe import print_function_d
 
 
 if __name__ == "__main__":
     print("Hello World!")
     print("I am checking for updates in a tests script")
+    
+    print()
+    print_function_d()
+    print()
+    
     check_for_update_and_restart()
